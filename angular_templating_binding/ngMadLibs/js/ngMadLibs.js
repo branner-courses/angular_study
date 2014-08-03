a46fe3571945522871978b45e3d8544e5a42d95f
David Branner <brannerchinese@gmail.com>
2014-08-03 01:41:22 -0400

    replace top-level ngMadLibs (was 1_3_6) with refactoring

--- angular_templating_binding/ngMadLibs/js/ngMadLibs.js
+++ angular_templating_binding/ngMadLibs/js/ngMadLibs.js
@@ -1,61 +1,54 @@
 // David Prager Branner
-// 20140730
+// 20140802
 
 angular.module('ngMadLibs', []).
   controller('Replacements', function($scope) {
+    $scope.origReplmnts = [
+      {name: 'Given_name', placeholder: 'Given name', type:'text'}, 
+      {name: 'Dirty_task', placeholder: 'Dirty task', type:'text'}, 
+      {name: 'Obnoxious_celebrity', placeholder: 'Obnoxious celebrity', type:'text'}, 
+      {name: 'Job_title', placeholder: 'Job title', type:'text'},   
+      {name: 'Celebrity', placeholder: 'Celebrity', type:'text'}, 
+      {name: 'Huge_number', placeholder: 'Huge number', type:'number'}, 
+      {name: 'Tedious_task', placeholder: 'Tedious task', type:'text'}, 
+      {name: 'Useless_skill', placeholder: 'Useless skill', type:'text'}, 
+      {name: 'Adjective', placeholder: 'Adjective', type:'text'}
+    ];
 
-    $scope.reset_userReplmnts = function() {
-      $scope.userReplmnts = origReplmnts.slice();
+    $scope.pronouns = {
+      unknown: {
+        subject: 'they', possessive: 'their', object: 'them'
+      },
+      male: {
+        subject: 'he', possessive: 'his', object: 'him'
+      },
+      female: {
+        subject: 'she', possessive: 'her', object: 'her'
+      },
     };
 
-    $scope.userInput = true;
-    $scope.reset_userReplmnts();
-
-    $scope.toggle = function() {
-      $scope.userInput = !$scope.userInput;
+    $scope.initialize_userReplmnts = function() {
+      $scope.userReplmnts = JSON.parse(JSON.stringify($scope.origReplmnts));
+      $scope.userInput = true;
     };
+
+    $scope.initialize_userReplmnts();
 
     $scope.generateMadLib = function() {
-      if ($scope.validate($scope.userReplments)) {
-        $scope.toggle();
-      }
+      $scope.userInput = false; 
     };
 
     $scope.resetMadLib = function() {
-      $scope.reset_userReplmnts();
-      $scope.toggle();
+      $scope.initialize_userReplmnts();
     };
 
     $scope.validate = function() {
-      console.log('$scope.userReplmnts: ' + $scope.userReplmnts);
-      // userReplmnts[5] must be number
-      if (isNaN($scope.userReplmnts[5])) {
-        alert('"' + origReplmnts[5] + '" must be a number.');
-        // Restore original placeholder.
-        $scope.userReplmnts[5] = origReplmnts[5];
-        return false;
-      }
-      // no element of $scope.userReplmnts can be the same as origReplmnts
-      else {
-        for (var i=0 ; i<origReplmnts.length ; i++) {
-          console.log(i, $scope.userReplmnts[i]);
-          if ($scope.userReplmnts[i] === origReplmnts[i]) {
-            alert('"' + origReplmnts[i] + '" must be changed.');
-            return false;
-          }
-          else if (!$scope.userReplmnts[i]) {
-            alert('"' + origReplmnts[i] + '" seems to be empty.');
-            // Restore original placeholder.
-            $scope.userReplmnts[i] = origReplmnts[i];
-            return false;
-          }
+      for (var i=0 ; i<$scope.userReplmnts.length ; i++) {
+        if ($scope.userReplmnts[i].$pristine) {
+          alert($scope.item + '" must be changed.');
+          return false;
         }
-      return true;
       }
+      return true;
     };
-  });
-
-origReplmnts = [
-   'Female_name', 'Dirty_task', 'Obnoxious_celebrity', 'Job_title', 
-   'Celebrity', 'Huge_number', 'Tedious_task', 'Useless_skill', 
-   'Adjective'];
+});
