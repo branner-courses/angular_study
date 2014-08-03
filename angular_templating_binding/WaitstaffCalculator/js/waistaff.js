2cf6a14819f6a8e1c91b8631dd1906856208bea6
David Branner <brannerchinese@gmail.com>
2014-08-03 17:18:12 -0400

    add working WaitstaffCalculator

---
+++ angular_templating_binding/WaitstaffCalculator/js/waistaff.js
@@ -0,0 +1,37 @@
+// David Prager Branner
+// 20140804
+
+angular.module('waitStaff', []).
+ controller('Calculator', function($scope) {
+
+  $scope.reset = function() {
+    $scope.custmrSubtotal = 0;
+    $scope.custmrTip = 0;
+    $scope.custmrTotal = 0;
+    $scope.tipTotal = 0;
+    $scope.mealCount = 0;
+    $scope.avgTip = 0;
+    $scope.cancel();
+  };
+
+  $scope.updateResult = function () {
+    // Customer charges
+    $scope.custmrSubtotal = $scope.baseMealPrice * (1 + ($scope.taxRate / 100));
+    $scope.custmrTip = $scope.custmrSubtotal * ($scope.tipPc / 100);
+    $scope.custmrTotal = $scope.custmrSubtotal + $scope.custmrTip;
+
+    // My earnings info
+    $scope.tipTotal = $scope.custmrTip + $scope.tipTotal;
+    $scope.mealCount++;
+    $scope.avgTip = $scope.tipTotal / $scope.mealCount;
+  };
+
+  $scope.cancel = function() {
+    $scope.baseMealPrice = '';
+    $scope.taxRate = '';
+    $scope.tipPc = '';
+  };
+
+  $scope.reset();
+});
+
